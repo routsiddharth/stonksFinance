ff615d442a5af3043bae3ef475ba03530738e543
Siddharth Rout <72611815+routsiddharth@users.noreply.github.com>
2022-05-31 20:54:42 +0300

    Made code more readable.

--- app.py
+++ app.py
@@ -43,30 +43,66 @@ cur = con.cursor()
 
 API_KEY = "pk_320446f105ef4f87839c51ec067b323d"
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
 
-def reformat(data, type):
-    if type == "users":
-        columns = ["id", "username", "hash", "cash"]
-    elif type == "transactions":
-        columns = ["id", "type", "ticker", "shares", "time", "share_price", "transaction_price"]
+    if request.method == "POST":
+
+        username = request.form.get("username").lower()
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username or not password or not confirmation:
+            return apology("All fields must be filled in")
+
+        if password != confirmation:
+            return apology("Password and confirmation do not match")
+
+        existing = reformat(cur.execute(f"SELECT * FROM users WHERE username = '{username}';"), "users")
+
+        if existing != []:
+            return apology("An account with this username already exists")
+
+        passwordhash = generate_password_hash(password)
+        cur.execute(f"INSERT INTO users (username, hash) VALUES ('{username}', '{passwordhash}');")
+        user_id = reformat(cur.execute(f"SELECT * FROM users WHERE username = '{username}';"), "users")[0]["id"]
+
+        cur.execute(
+            f"CREATE TABLE owned{user_id} (ticker TEXT, shares INTEGER, buy_price NUMERIC, current_price NUMERIC, pl NUMERIC)")
+        return redirect("/login")
+
     else:
-        columns = ["ticker", "shares", "buy_price", "current_price", "pl"]
+        return render_template("register.html")
 
-    returned = []
 
-    for line in data:
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
 
-        d = {}
+    # Forget any user_id
+    session.clear()
 
-        for i in range(len(line)):
-            try:
-                d[columns[i]] = round(line[i], 2)
-            except:
-                d[columns[i]] = line[i]
+    if request.method == "POST":
 
-        returned.append(d)
+        username = request.form.get("username").lower()
+        password = request.form.get("password")
 
-    return returned
+        if not username or not password:
+            return apology("Please enter a valid username and password")
+
+        rows = reformat(cur.execute(f"SELECT * FROM users WHERE username = '{username}'"), "users")
+
+        print(rows)
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+            return apology("Invalid username and/or password")
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
 
 
 @app.route("/")
@@ -108,6 +144,95 @@ def index():
     totalHoldings += cash
 
     return render_template("index.html", username=username, portfolio=portfolio, cash=cash, totalHoldings=totalHoldings)
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+
+    global chartTicker, chartTimeframe
+
+    if request.method == "POST":
+
+        ticker = request.form.get("symbol")
+        timeframe = request.form.get("timeframe")
+
+        if not ticker or not timeframe:
+            return apology("Please fill in both fields")
+
+        response = requests.get(f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={API_KEY}")
+
+        if response.status_code != 200:
+            return apology("Please enter a valid stock ticker")
+
+        stockData = response.json()
+
+    else:
+
+        stockData = requests.get(f"https://cloud.iexapis.com/stable/stock/nflx/quote?token={API_KEY}").json()
+        timeframe = 30
+
+    values = {}
+
+    values["company"] = stockData["companyName"]
+    values["ticker"] = stockData["symbol"]
+    values["price"] = stockData["latestPrice"]
+
+    marketCap = stockData["marketCap"]
+
+    if marketCap > 1000000000000:
+        marketCap = f"{round(marketCap / 1000000000000, 2)} trillion USD"
+    elif marketCap > 1000000000:
+        marketCap = f"{round(marketCap / 1000000000, 2)} billion USD"
+    elif marketCap > 1000000:
+        marketCap = f"{round(marketCap / 1000000, 2)} million USD"
+    elif marketCap > 1000:
+        marketCap = f"{round(marketCap / 1000, 2)} thousand USD"
+
+    values["marketCap"] = marketCap
+
+    chartTicker = values["ticker"]
+    chartTimeframe = timeframe
+
+    return render_template("quote.html", values=values)
+
+@app.route('/plot.png')
+def plot_png():
+
+    fig = create_figure()
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+
+    historical = requests.get(
+        f"http://api.marketstack.com/v1/eod?access_key=229c65f839b3fc236d170ccf290f2a82&symbols={chartTicker}&limit={chartTimeframe}").json()["data"]
+
+    xVals = []
+    yVals = []
+
+    count = 1
+
+    for stat in historical:
+
+        xVals.append(-count)
+        yVals.append(float(stat["adj_close"]))
+
+        count += 1
+
+    axis.plot(xVals, yVals)
+
+    fig.suptitle(f"{chartTicker} Price: {chartTimeframe} days")
+
+    axis.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+
+    return fig
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -163,176 +288,7 @@ def buy():
         return redirect("/")
 
     else:
-        return render_template("buy.html")
-
-
-@app.route("/history")
-@login_required
-def history():
-
-    user = session["user_id"]
-    userHistory = reformat(cur.execute(f"SELECT * FROM transactions WHERE id = {user}"), "transactions")[::-1]
-
-    return render_template("history.html", transactions=userHistory)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
-    if request.method == "POST":
-
-        username = request.form.get("username").lower()
-        password = request.form.get("password")
-
-        if not username or not password:
-            return apology("Please enter a valid username and password")
-
-        rows = reformat(cur.execute(f"SELECT * FROM users WHERE username = '{username}'"), "users")
-
-        print(rows)
-
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
-            return apology("Invalid username and/or password")
-
-        session["user_id"] = rows[0]["id"]
-
         return redirect("/")
-
-    else:
-        return render_template("login.html")
-
-
-@app.route("/logout")
-def logout():
-
-    session.clear()
-    return redirect("/login")
-
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-
-    global chartTicker, chartTimeframe
-
-    if request.method == "POST":
-
-        ticker = request.form.get("symbol")
-        timeframe = request.form.get("timeframe")
-
-        if not ticker or not timeframe:
-            return apology("Please fill in both fields")
-
-        response = requests.get(f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={API_KEY}")
-
-        if response.status_code != 200:
-            return apology("Please enter a valid stock ticker")
-
-        stockData = response.json()
-
-    else:
-
-        stockData = requests.get(f"https://cloud.iexapis.com/stable/stock/nflx/quote?token={API_KEY}").json()
-        timeframe = 30
-
-    values = {}
-
-    values["company"] = stockData["companyName"]
-    values["ticker"] = stockData["symbol"]
-    values["price"] = stockData["latestPrice"]
-
-    marketCap = stockData["marketCap"]
-
-    if marketCap > 1000000000000:
-        marketCap = f"{round(marketCap / 1000000000000, 2)} trillion USD"
-    elif marketCap > 1000000000:
-        marketCap = f"{round(marketCap / 1000000000, 2)} billion USD"
-    elif marketCap > 1000000:
-        marketCap = f"{round(marketCap / 1000000, 2)} million USD"
-    elif marketCap > 1000:
-        marketCap = f"{round(marketCap / 1000, 2)} thousand USD"
-
-    values["marketCap"] = marketCap
-
-    chartTicker = values["ticker"]
-    chartTimeframe = timeframe
-
-    return render_template("quote.html", values=values)
-
-
-@app.route('/plot.png')
-def plot_png():
-
-    fig = create_figure()
-
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-
-def create_figure():
-
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-
-    historical = requests.get(
-        f"http://api.marketstack.com/v1/eod?access_key=229c65f839b3fc236d170ccf290f2a82&symbols={chartTicker}&limit={chartTimeframe}").json()["data"]
-
-    xVals = []
-    yVals = []
-
-    count = 1
-
-    for stat in historical:
-
-        xVals.append(-count)
-        yVals.append(float(stat["adj_close"]))
-
-        count += 1
-
-    axis.plot(xVals, yVals)
-
-    fig.suptitle(f"{chartTicker} Price: {chartTimeframe} days")
-
-    axis.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-
-    return fig
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-
-    if request.method == "POST":
-
-        username = request.form.get("username").lower()
-        password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-
-        if not username or not password or not confirmation:
-            return apology("All fields must be filled in")
-
-        if password != confirmation:
-            return apology("Password and confirmation do not match")
-
-        existing = reformat(cur.execute(f"SELECT * FROM users WHERE username = '{username}';"), "users")
-
-        if existing != []:
-            return apology("An account with this username already exists")
-
-        passwordhash = generate_password_hash(password)
-        cur.execute(f"INSERT INTO users (username, hash) VALUES ('{username}', '{passwordhash}');")
-        user_id = reformat(cur.execute(f"SELECT * FROM users WHERE username = '{username}';"), "users")[0]["id"]
-
-        cur.execute(
-            f"CREATE TABLE owned{user_id} (ticker TEXT, shares INTEGER, buy_price NUMERIC, current_price NUMERIC, pl NUMERIC)")
-        return redirect("/login")
-
-    else:
-        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
@@ -379,7 +335,23 @@ def sell():
 
     else:
 
-        return render_template("sell.html")
+        return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+
+@app.route("/history")
+@login_required
+def history():
+
+    user = session["user_id"]
+    userHistory = reformat(cur.execute(f"SELECT * FROM transactions WHERE id = {user}"), "transactions")[::-1]
+
+    return render_template("history.html", transactions=userHistory)
 
 
 @app.route("/users", methods=["GET", "POST"])
@@ -395,12 +367,58 @@ def users():
 
         user_id = response[0]["id"]
 
-        transactions = reformat(cur.execute(f"SELECT * FROM owned{user_id};"), "owned")
+        owned = reformat(cur.execute(f"SELECT * FROM owned{user_id};"), "owned")
 
-        return render_template("userinfo.html", user=response[0], transactions=transactions)
+        for stock in owned:
+
+            ticker = stock['ticker']
+
+            if ticker == "AAAA":
+                currentPrice = 28
+
+            else:
+                currentPrice = float(requests.get(
+                    f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={API_KEY}").json()["latestPrice"])
+
+            buyPrice = float(stock['buy_price'])
+            pl = round((currentPrice - buyPrice)/buyPrice * 100, 2)
+
+            cur.execute(f"UPDATE owned{user_id} SET current_price = {currentPrice}, pl = {pl} WHERE ticker = '{ticker}';")
+
+
+        owned = reformat(cur.execute(f"SELECT * FROM owned{user_id};"), "owned")
+
+        # When passed into the template, variable is called transactions but it isn't transactions!
+        # It is current holdings. There is an issue with any other variable name, trying to fix it.
+        return render_template("userinfo.html", user=response[0], transactions=owned)
 
     else:
         return render_template("searchusers.html")
+
+
+def reformat(data, type):
+    if type == "users":
+        columns = ["id", "username", "hash", "cash"]
+    elif type == "transactions":
+        columns = ["id", "type", "ticker", "shares", "time", "share_price", "transaction_price"]
+    else:
+        columns = ["ticker", "shares", "buy_price", "current_price", "pl"]
+
+    returned = []
+
+    for line in data:
+
+        d = {}
+
+        for i in range(len(line)):
+            try:
+                d[columns[i]] = round(line[i], 2)
+            except:
+                d[columns[i]] = line[i]
+
+        returned.append(d)
+
+    return returned
 
 
 def errorhandler(e):
